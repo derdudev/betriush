333a70bbf2b04f5f01780d238fa328a75c63e63e
MDev <46203814+Matteo-Dev@users.noreply.github.com>
2022-10-19 12:13:36 +0200

    initial commit

---
+++ src/main/java/com/sexydari/betriush/BetriushApplication.java
@@ -0,0 +1,13 @@
+package com.sexydari.betriush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetriushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetriushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sexydari/betriush/BetriushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sexydari.betriush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetriushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
