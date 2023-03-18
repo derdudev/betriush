f9b25d5fa5399e483cec4d61444ff3c26cc63141
MDev <46203814+Matteo-Dev@users.noreply.github.com>
2023-03-18 16:29:44 +0100

    getting response but everything is blocked

--- src/main/java/com/sexydari/betriush/auth/jwt/AuthTokenFilter.java
+++ src/main/java/com/sexydari/betriush/auth/jwt/AuthTokenFilter.java
@@ -45,6 +45,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         } catch(Exception e){
             logger.error("Cannot set user authentication: {}", e);
         }
+        filterChain.doFilter(request, response);
     }
 
     private String parseJwt(HttpServletRequest request){
